4005b333a963385e7bb4cd99dc3cb247b1a78a5a
Neenus Gabriel <hello@neenus.com>
2022-02-14 02:26:01 -0500

    chore(react-router): update react-router-dom to version 6.2.1 from 5.2.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Container } from "@mui/material";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
 import AboutPage from "./pages/aboutpage/AboutPage";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <React.Fragment>
-          <Container className="App">
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/about" component={AboutPage} />
-          </Container>
-          <Route />
-        </React.Fragment>
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Container className="App">
+        <Routes>
+          <React.Fragment>
+            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/about" element={<AboutPage />} />
+            <Route />
+          </React.Fragment>
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 
